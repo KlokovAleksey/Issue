d13ab94ec0782975e789d23460112a09a65532eb
Aleksey Klokov <lex-oz@yandex.ru>
2021-01-25 17:31:05 +0300

    modified IssueManagerTest

--- src/test/java/domain/IssueManagerTest.java
+++ src/test/java/domain/IssueManagerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repository.IssueRepository;
 
-;
 import java.util.*;
 import java.util.List;
 
@@ -16,12 +15,12 @@ class IssueManagerTest {
     private IssueManager manager = new IssueManager(repository);
 
 
-    private Issue issue1 = new Issue(1, true, "Andrey",new HashSet(Collections.singleton("bug")),new HashSet(Collections.singleton("Nikolay")));
-    private Issue issue2 = new Issue(2, false, "Anna", new HashSet(Collections.singleton("question")), new HashSet(Collections.singleton("Sergey")));
-    private Issue issue3 = new Issue(3, true, "Sergey", new HashSet(Collections.singleton("documentation")), new HashSet(Collections.singleton("Andrey")));
-    private Issue issue4 = new Issue(4, true, "Anna",new HashSet(Collections.singleton("question")), new HashSet(Collections.singleton("Nikolay")));
-    private Issue issue5 = new Issue(5, true, "Nikolay", new HashSet(Collections.singleton("bug")), new HashSet(Collections.singleton("Sergey")));
-    private Issue issue6 = new Issue(6, false, "Andrey", new HashSet(Collections.singleton("bug")), new HashSet(Collections.singleton("Anna")));
+    private Issue issue1 = new Issue(1, true, "Andrey",new HashSet<String>(Arrays.asList("bug")),new HashSet<String>(Arrays.asList("Nikolay")));
+    private Issue issue2 = new Issue(2, false, "Anna", new HashSet<String>(Arrays.asList("question")), new HashSet<String>(Arrays.asList("Sergey")));
+    private Issue issue3 = new Issue(3, true, "Sergey", new HashSet<String>(Arrays.asList("documentation")), new HashSet<String>(Arrays.asList("Andrey")));
+    private Issue issue4 = new Issue(4, true, "Anna",new HashSet<String>(Arrays.asList("question")), new HashSet<String>(Arrays.asList("Nikolay")));
+    private Issue issue5 = new Issue(5, true, "Nikolay", new HashSet<String>(Arrays.asList("bug")), new HashSet<String>(Arrays.asList("Sergey")));
+    private Issue issue6 = new Issue(6, false, "Andrey", new HashSet<String>(Arrays.asList("bug")), new HashSet<String>(Arrays.asList("Anna")));
 
 
     @BeforeEach
